730f86f54063e8841211426bc42be58812bcd7dc
Guadman98 <guadmanmen0722@gmail.com>
2024-05-30 15:15:51 -0500

    Initial commit for application CRUD heroes

---
+++ src/app/auth/pages/resgister-page/resgister-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resgister-page',
+  templateUrl: './resgister-page.component.html',
+  styles: ``
+})
+export class ResgisterPageComponent {
+
+}
